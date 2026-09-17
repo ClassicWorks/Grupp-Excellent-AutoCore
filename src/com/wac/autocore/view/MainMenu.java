9f477a20aacd021c94e8b2f362d9e499314c08a8
Sara Nilsson <nilssonsara97@gmail.com>
2026-09-17 14:08:27 +0200

    packages update

--- src/com/wac/autocore/view/MainMenu.java
+++ src/com/wac/autocore/view/MainMenu.java
@@ -1,4 +1,13 @@
 package com.wac.autocore.view;
 
 public class MainMenu {
+    /*customer
+    * Vehicles
+    * Bookings
+    * Mechanics
+    * Services
+    * work orders
+    * invoices
+    * Payments
+    * Exit*/
 }
